7985ca7965a7db3c4a54d648fff413afc2b6d41a
elboraki <younes.elboraki@gmail.com>
2021-11-28 21:56:29 +0100

    implemented edit password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def set_current_user
     Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+  def required_user_logged_in!
+    redirect_to root_path, alert: 'You should login' if Current.user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   post 'sign_up', to: 'registrations#create'
 
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update"
+
+
   delete 'logout', to: 'sessions#destroy'
   get "sign_in" , to: "sessions#new"
   post "sign_in", to: "sessions#create"
